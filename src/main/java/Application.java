baed5df307a70bc8649eb2dbbbc278fb1635b7ca
acarolinacc <acarolinacc2003@gmail.com>
2022-10-05 17:44:15 +0100

    Parte 6

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,23 +8,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) {
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.run();
     }
 }
